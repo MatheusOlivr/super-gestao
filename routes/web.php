a9262482fca524449f1f17260b620439c0ffb9c0
MatheusOlivr <contato.matheusoliv@gmail.com>
2022-06-03 11:00:07 -0300

    Change laravel version and create the principal action of PrincipalController

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "Hello, Welcome to main page";
-});
-Route::get('/sobre-nos',function() {
-    return "sobre-nos";
+Route::get('/',"PrincipalController@principal");
+Route::get('/sobreNos',function() {
+    return "sobreNos";
 });
 Route::get("contato",function() {
     return "contato";
